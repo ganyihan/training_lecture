73f2c995ee20be9dc9af5487c86b75286c92bc07
chenyiwei <2339168258@qq.com>
2022-08-31 14:39:44 +0800

    用户持股

---
+++ src/main/java/com/example/business/entity/UserStockView.java
@@ -0,0 +1,36 @@
+package com.example.business.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ * VIEW
+ * </p>
+ *
+ * @author cyw
+ * @since 2022-08-30
+ */
+@TableName("user_stock_view")
+@ApiModel(value = "UserStockView对象", description = "VIEW")
+@Data
+public class UserStockView implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty("用户id")
+    private Integer userId;
+
+    @ApiModelProperty("股票代码")
+    private String ric;
+
+    @ApiModelProperty("股票名称")
+    private String ticker;
+
+    private Integer size;
+
+}
